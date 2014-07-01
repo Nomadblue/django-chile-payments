03c21ff1432a9adcffc7e62bf4593a1b5b1677a3
José Sazo <jose.sazo@gmail.com>
2014-07-01 10:12:44 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='django-chile-payments',
+    description='Chilean payment brokers for Django',
+    long_description=long_description,
+    version='0.1.1',
+    packages=find_packages(),
+    url='https://github.com/Nomadblue/django-chile-payments',
+    license='MIT',
+    author='José Sazo',
+    author_email='jose.sazo@gmail.com',
+    include_package_data=True,
+)
