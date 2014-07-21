35f6688fd3491d0bd346701a7452afd73cca8108
José Sazo <jose.sazo@gmail.com>
2014-07-21 17:31:31 -0400

    minor fixes

--- getpaid/backends/paypal/__init__.py
+++ getpaid/backends/paypal/__init__.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from getpaid.backends import PaymentProcessorBase
+from paypal.pro import signals as wpp_signals
 
 
 class PaymentProcessor(PaymentProcessorBase):
@@ -9,4 +10,20 @@ class PaymentProcessor(PaymentProcessorBase):
     BACKEND_ACCEPTED_CURRENCY = ('USD',)
 
     def get_gateway_url(self, request):
-        return reverse('getpaid-paypal-payment', kwargs={'pk': self.payment.pk}), "GET", {}
+        return reverse('getpaid-paypal-authorization', kwargs={'pk': self.payment.pk}), "GET", {}
+
+
+def payment_successful(sender, *args, **kwargs):
+    from getpaid.models import Payment
+    payment = Payment.objects.get(pk=kwargs['invnum'])
+    payment.on_success()
+
+
+def payment_flagged(sender, *args, **kwargs):
+    from getpaid.models import Payment
+    payment = Payment.objects.get(pk=kwargs['invnum'])
+    payment.on_failure()
+
+
+wpp_signals.payment_was_successful.connect(payment_successful)
+wpp_signals.payment_was_flagged.connect(payment_flagged)
--- getpaid/backends/paypal/views.py
+++ getpaid/backends/paypal/views.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.shortcuts import get_object_or_404
 from getpaid.models import Payment
 from paypal.pro.views import PayPalPro
@@ -5,14 +6,19 @@ from paypal.pro.views import PayPalPro
 
 def authorization(request, pk):
     payment = get_object_or_404(Payment, pk=pk)
+    TWOPLACES = Decimal(10) ** -2
 
-    item = {'amt': payment.amount,
-            'invnum': payment.pk,}
+    amount = str(payment.amount.quantize(TWOPLACES))
+
+    item = {'amt': amount,
+            'invnum': payment.pk,
+            'cancelurl': payment.order.get_absolute_url(),
+            'returnurl': payment.order.get_absolute_url()}
 
     kw = {'item': item,
           'payment_template': 'getpaid/paypal_payment.html',
           'confirm_template': 'getpaid/paypal_confirm.html',
-          'success_url': '/success/',  # FIX
-          'fail_url': '/fail/'}  #FIX
+          'success_url': payment.order.get_absolute_url(),  # FIX
+          'fail_url': payment.order.get_absolute_url()}  # FIX
     ppp = PayPalPro(**kw)
     return ppp(request)
